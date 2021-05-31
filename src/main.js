0a5f204ec0cedbe0777e4ca28b6dd8fbe392ebf6
bulbulica2 <necrocris2000@yahoo.com>
2021-05-31 09:59:43 +0300

    Small improvements on indentation

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import WebApp from '@/web/App.vue';
 import DesktopApp from '@/desktop/App.vue';
 import router from '@/web/router';
 import store from '@/web/store';
+// @ is an alias to /src
 
 // check for desktop or web app
 if (isElectron()) {
--- src/web/views/Home.vue
+++ src/web/views/Home.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/web/components/HelloWorld.vue';
 
 export default {
